38691c27fe8e63207eb75fc378cfaddfd0e71ab2
Abdulaziz2666 <abdulazizshermatov10@gmail.com>
2026-07-24 22:42:29 +0500

    Integrate pytesseract for OCR and version display
    Added OCR functionality using pytesseract and version check.

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import pytesseract
 
+print("OCR MODUL ISHLAYAPTI ✅")
+
+try:
+    version = pytesseract.get_tesseract_version()
+    print("Tesseract versiyasi:", version)
+except Exception as e:
+    print("Tesseract topilmadi:", e)
